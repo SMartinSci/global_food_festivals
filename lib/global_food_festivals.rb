400bc97d722c7f4fb282799cefee5c665275dec8
Sherry M <40738084+SMartinSci@users.noreply.github.com>
2018-10-09 16:22:46 -0400

    Update global_food_festivals.rb

--- lib/global_food_festivals.rb
+++ lib/global_food_festivals.rb
@@ -3,5 +3,5 @@ require 'pry'
 require 'open-uri'
 require 'nokogiri'
 
-require_relative "./global_food_festivals/version"
-require_relative "./global_food_festivals/cli"
+require_relative "global_food_festivals/version"
+require_relative "global_food_festivals/cli"
