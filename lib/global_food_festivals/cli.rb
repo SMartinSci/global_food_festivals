aa7bcc5547d5fd6e307999a59f3be77501fe0f4a
Sherry Martin <sm674@cornell.edu>
2018-10-09 19:10:02 +0000

    Modify cli.rb to include food festivals

--- lib/global_food_festivals/cli.rb
+++ lib/global_food_festivals/cli.rb
@@ -1,7 +1,19 @@
+#CLI Controller
+
 class GlobalFoodFestivals::CLI
 
   def call
     puts "Global Food Festivals:"
+    list_festivals
   end
 
+  def list_festivals
+    puts <<- DOC
+    1.Tasting Australia – South Australia
+    2.Venice Food & Wine Festival – 	Venice, Italy
+    3.Vegas Uncork’d – Las Vegas, 	Nevada
+    4.… etc.
+    # get festivals
+    DOC
+  end
 end
