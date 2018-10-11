a7e564e006aded5550a201c5dbe31460b1705c3c
Sherry Martin <sm674@cornell.edu>
2018-10-11 00:36:21 +0000

    Update cli with scraper

--- lib/global_food_festivals.rb
+++ lib/global_food_festivals.rb
@@ -1,4 +1,8 @@
 #Environment
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative "global_food_festivals/version"
 require_relative "global_food_festivals/cli"
 require_relative "global_food_festivals/festival"
--- lib/global_food_festivals/festival.rb
+++ lib/global_food_festivals/festival.rb
@@ -1,26 +1,20 @@
 class GlobalFoodFestivals::Festival
   attr_accessor :name, :url, :location, :description
   def self.complete
-    # Return a bunch of instances of festivals
-    # puts <<- DOC.gsub /^\s*/, ''
-    # 1.Tasting Australia – South Australia
-    # 2.Venice Food & Wine Festival – 	Venice, Italy
-    # 3.Vegas Uncork’d – Las Vegas, 	Nevada
-    # 4.… etc.
-    #   #get festivals
-    # DOC
-    festival_1 = self.new
-    festival_1.name = "Tasting Australia"
-    festival_1.url = "https://www.fodors.com/world/australia-and-the-pacific/australia/south-australia"
-    festival_1.location = "South Australia"
-    festival_1.description = "Home to some of Australia’s most prominent wine-growing regions, wide swaths of the Outback and such biodiverse destinations
-    as Kangaroo Island and the Limestone Coast, it would feasibly take months (or even years) to explore the reaches of South Australia‘s gastronomy and
-    viticulture—not to mention its many scenic regions. Thankfully, Tasting Australia condenses many of the state’s food and wine highlights into a robust
-    10-day program of more than 140 events—farm tours, wine, beer, and spirits tastings and masterclasses, leisurely chef-driven lunches and dinners included.
-    Festival highlights include the Glasshouse Kitchen in the capital city of Adelaide, where lauded Australian chefs and international culinary stars team up
-    for some memorable meals, and Tasting Australia Airlines, which transports visitors to some of the state’s more remote culinary destinations on all-inclusive
-    full-day tours."
+    self.scrape_complete
+  end
 
-    [festival_1]
+
+  def self.scrape_festivals
+    #Go to fodors, find the festival, extract the properties, instantiate a festival
+    festivals = []
+
+festivals << self.scrape_fodors
+    festival_1 = self.new
+    festivals
+  end
+
+  def self.scrape_fodors
+    doc = Nokogiri::HTML(open("https://www.fodors.com"))
   end
 end
