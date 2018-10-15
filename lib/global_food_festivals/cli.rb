c48580351f99c1862c9fe13027124b86f4fa8fd4
Sherry Martin <sm674@cornell.edu>
2018-10-15 15:23:40 +0000

    Range operator insertion in CLI

--- lib/global_food_festivals/cli.rb
+++ lib/global_food_festivals/cli.rb
@@ -22,7 +22,7 @@ class CLI
     while input != "exit"
       input = gets.strip.downcase
 
-      if input.to_i > 0 && input.to_i < 13 #If greater than 0 treat as integer, not a string
+      if input.to_i.between?(1, 12) #If greater than 0 treat as integer, not a string
         the_festival = @all_festivals[input.to_i-1]
         puts "#{the_festival.name}\n\n"
         puts "#{the_festival.location}\n\n"
