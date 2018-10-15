86fcc11821e101a8e4a84b755db91b817986129d
Sherry Martin <sm674@cornell.edu>
2018-10-15 14:16:57 +0000

    Update user message CLI

--- lib/global_food_festivals/cli.rb
+++ lib/global_food_festivals/cli.rb
@@ -8,7 +8,9 @@ class CLI
   end
 
   def list_festivals
-    puts "Global Food Festivals:"
+    puts "Welcome to 12 Global Food Festivals worth the trip.\n"
+    puts "Which food festivals would you like to see more information?"
+    puts "Enter the cooresponding number, listed below, of each food festival to get a brief description and location."
     @all_festivals = Festival.scrape_fodors
     @all_festivals.each.with_index(1) do |festival, i| #using 1 eliminates the need to subtract 1 from the index
     puts "#{i}. #{festival.name}\n\n"
@@ -25,7 +27,7 @@ class CLI
         the_festival = @all_festivals[input.to_i-1]
         puts "#{the_festival.name}\n\n"
         puts "#{the_festival.location}\n\n"
-        puts "#{the_festival.description}"
+        puts "#{the_festival.description}\n\n"
       elsif input == "list"
         list_festivals
       else
