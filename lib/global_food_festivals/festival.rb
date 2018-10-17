35ab156751f5052bee756c0b2d01762cb2649f9a
Sherry Martin <sm674@cornell.edu>
2018-10-17 15:13:29 +0000

    Update festival file, remove scraper method to add to it's own Class.

--- lib/global_food_festivals/festival.rb
+++ lib/global_food_festivals/festival.rb
@@ -13,14 +13,4 @@ class Festival
     @description = description
     @@all << self
   end
-
-  def self.scrape_fodors
-    doc = Nokogiri::HTML(open("https://www.fodors.com/news/photos/12-global-food-festivals-worth-the-trip"))
-    links = doc.search(".container.slides").map do |festival|
-    name = festival.css("h2").text.gsub("Book a Hotel", " ").delete!("\n").strip
-    location = festival.css("h3 span").text.delete!("\n").strip
-    description = festival.css("p").map(&:text).reject(&:empty?).join("\n\n")
-    Festival.new(name, location, description)
-    end
-  end
 end
