fb15d23540c96e0491ce9014b5e41108609107c8
Sherry Martin <sm674@cornell.edu>
2018-10-18 16:35:46 +0000

    Add notes to festival.rb file

--- lib/global_food_festivals/festival.rb
+++ lib/global_food_festivals/festival.rb
@@ -6,12 +6,13 @@ class Festival
 
   def self.all
     @@all
+    binding.pry
   end
 
   def initialize(name, location, description) #Each instance variable below, returns an instance of each attribute. e.g. @name = "Venice Food and Wine Festival"
     @name = name
     @location = location
     @description = description
-    @@all << self  #self returns 1 instance of Festival/1 Festival ID, location, name, description; @@all returns all instances ID's and attributes
+    @@all << self  #self returns 1 instance of Festival/1 Festival ID, location, name, description; @@all returns all instances' ID's and attributes
   end
 end
